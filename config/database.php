c43c9c588a100e3a8d233427cb6fbe5434320e71
Oleksha <deryabino@gmail.com>
2023-10-14 23:38:45 +0400

    Добавили вывод статьи из БД

---
+++ config/database.php
@@ -0,0 +1,8 @@
+<?php
+
+// массив настроек для соединения с БД
+return [
+  'dsn' => 'mysql:host=localhost;dbname=blog.loc;charset=utf8',
+  'user' => 'root',
+  'pass' => '',
+];
--- index.php
+++ index.php
@@ -1,4 +1,6 @@
 <?php
+
+use Blog\PostMapper;
 use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
 use Slim\Factory\AppFactory;
@@ -10,7 +12,23 @@ require __DIR__ . '/vendor/autoload.php';
 $loader = new FilesystemLoader('templates');
 $view = new Environment($loader);
 
+$config = include 'config/database.php';
+$dsn = $config['dsn'];
+$user = $config['user'];
+$pass = $config['pass'];
+
+try {
+  $connection = new PDO($dsn, $user, $pass);
+  $connection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+  $connection->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
+} catch (PDOException $exception) {
+  echo 'Ошибка подключения к БД: ' . $exception->getMessage();
+  die;
+}
+
 $app = AppFactory::create();
+
+$postMapper = new PostMapper($connection);
 
 $app->get('/', function (Request $request, Response $response, $args) use ($view) {
   $body = $view->render('index.twig');
@@ -25,9 +43,16 @@ $app->get('/about', function (Request $request, Response $response, $args) use (
   return $response;
 });
 $app->get('/{url_key}', function (Request $request, Response $response, $args) use ($view) {
-  $body = $view->render('post.twig', [
-    'url_key' => $args['url_key']
-  ]);
+  global $postMapper;
+  $post = $postMapper->getByUrlKey((string) $args['url_key']);
+  if (empty($post)) {
+    $body = $view->render('not-found.twig');
+  } else {
+    $body = $view->render('post.twig', [
+      'post' => $post
+    ]);
+  }
+
   $response->getBody()->write($body);
   return $response;
 });
